95b836c19a3ab533d38322e5c5851f16fc2e28d5
tiendung85 <tien.dungg2011@gmail.com>
2026-05-31 03:28:41 +0700

    update

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,9 +1,11 @@
-﻿import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+
 import ShopLandingPage from './pages/ShopLandingPage'
 import LoginPage from './pages/LoginPage'
 import ForgotPasswordPage from './pages/ForgotPasswordPage'
 import ResetPasswordPage from './pages/ResetPasswordPage'
 import RegisterPage from './pages/RegisterPage'
+
 import ShopDashboardPage from './pages/ShopDashboardPage'
 import ShopBookingsPage from './pages/ShopBookingsPage'
 import ShopWalletPage from './pages/ShopWalletPage'
@@ -13,32 +15,34 @@ import ShopDepositConfigPage from './pages/ShopDepositConfigPage'
 import ShopSlotsConfigPage from './pages/ShopSlotsConfigPage'
 import ShopStatisticsPage from './pages/ShopStatisticsPage'
 import ShopCreateBookingPage from './pages/ShopCreateBookingPage'
-import AdminDashboardPage from './pages/AdminDashboardPage'
-import AdminPartnersPage from './pages/AdminPartnersPage'
-import AdminApprovalsPage from './pages/AdminApprovalsPage'
-import AdminFinancePage from './pages/AdminFinancePage'
-import AdminRiskPage from './pages/AdminRiskPage'
-import AdminSupportPage from './pages/AdminSupportPage'
-import AdminSettingsPage from './pages/AdminSettingsPage'
-import AdminRefundsPage from './pages/AdminRefundsPage'
-import AdminPartnerDetailPage from './pages/AdminPartnerDetailPage'
-import AdminApprovalDetailPage from './pages/AdminApprovalDetailPage'
-import AdminTicketDetailPage from './pages/AdminTicketDetailPage'
-import AdminIncidentDetailPage from './pages/AdminIncidentDetailPage'
-import AdminBookingDetailPage from './pages/AdminBookingDetailPage'
-import { ShopProvider } from './context/ShopContext'
 import ShopOnboardingPage from './pages/ShopOnboardingPage'
 import ShopBookingDetailPage from './pages/ShopBookingDetailPage'
 import ShopConfigPage from './pages/ShopConfigPage'
+
+import AdminDashboardPage from './pages/AdminDashboardPage'
+import AdminPartnersPage from './pages/AdminPartnersPage'
+import AdminPartnerDetailPage from './pages/AdminPartnerDetailPage'
+import AdminApprovalsPage from './pages/AdminApprovalsPage'
+import AdminApprovalDetailPage from './pages/AdminApprovalDetailPage'
+import AdminFinancePage from './pages/AdminFinancePage'
+import AdminRiskPage from './pages/AdminRiskPage'
+import AdminIncidentDetailPage from './pages/AdminIncidentDetailPage'
+import AdminSupportPage from './pages/AdminSupportPage'
+import AdminTicketDetailPage from './pages/AdminTicketDetailPage'
+import AdminSettingsPage from './pages/AdminSettingsPage'
+import AdminRefundsPage from './pages/AdminRefundsPage'
+import AdminBookingDetailPage from './pages/AdminBookingDetailPage'
+
 import CustomerHomePage from './pages/CustomerHomePage'
 import CustomerServiceDetailPage from './pages/CustomerServiceDetailPage'
 import CustomerSelectServicePage from './pages/CustomerSelectServicePage'
 import CustomerBookingTimePage from './pages/CustomerBookingTimePage'
 import CustomerPaymentPage from './pages/CustomerPaymentPage'
-import CustomerAppointmentsPage from './pages/CustomerAppointmentsPage'
 import CustomerAccountBookingsPage from './pages/CustomerAccountBookingsPage'
 import CustomerRegisterPage from './pages/CustomerRegisterPage'
 import CustomerProfilePage from './pages/CustomerProfilePage'
+
+import { ShopProvider } from './context/ShopContext'
 import { GuestOnly, RequireRole } from './components/auth/RouteGuards'
 import { ToastProvider } from './components/ui/ToastProvider'
 
@@ -49,11 +53,13 @@ function App() {
         <Router future={{ v7_startTransition: true, v7_relativeSplatPath: true }}>
           <Routes>
             <Route path="/" element={<ShopLandingPage />} />
+
             <Route path="/login" element={<GuestOnly><LoginPage /></GuestOnly>} />
             <Route path="/register" element={<GuestOnly><RegisterPage /></GuestOnly>} />
             <Route path="/customer/register" element={<GuestOnly><CustomerRegisterPage /></GuestOnly>} />
             <Route path="/forgot-password" element={<GuestOnly><ForgotPasswordPage /></GuestOnly>} />
             <Route path="/reset-password/:token" element={<GuestOnly><ResetPasswordPage /></GuestOnly>} />
+
             <Route path="/shop/dashboard" element={<RequireRole allow={['shop']}><ShopDashboardPage /></RequireRole>} />
             <Route path="/shop/onboarding" element={<RequireRole allow={['shop']}><ShopOnboardingPage /></RequireRole>} />
             <Route path="/shop/bookings" element={<RequireRole allow={['shop']}><ShopBookingsPage /></RequireRole>} />
@@ -66,6 +72,7 @@ function App() {
             <Route path="/shop/config/slots" element={<RequireRole allow={['shop']}><ShopSlotsConfigPage /></RequireRole>} />
             <Route path="/shop/config/shop" element={<RequireRole allow={['shop']}><ShopConfigPage /></RequireRole>} />
             <Route path="/shop/statistics" element={<RequireRole allow={['shop']}><ShopStatisticsPage /></RequireRole>} />
+
             <Route path="/admin/dashboard" element={<RequireRole allow={['admin']}><AdminDashboardPage /></RequireRole>} />
             <Route path="/admin/bookings/:id" element={<RequireRole allow={['admin']}><AdminBookingDetailPage /></RequireRole>} />
             <Route path="/admin/partners" element={<RequireRole allow={['admin']}><AdminPartnersPage /></RequireRole>} />
@@ -78,14 +85,15 @@ function App() {
             <Route path="/admin/support" element={<RequireRole allow={['admin']}><AdminSupportPage /></RequireRole>} />
             <Route path="/admin/support/:id" element={<RequireRole allow={['admin']}><AdminTicketDetailPage /></RequireRole>} />
             <Route path="/admin/settings" element={<RequireRole allow={['admin']}><AdminSettingsPage /></RequireRole>} />
+            <Route path="/admin/refunds" element={<RequireRole allow={['admin']}><AdminRefundsPage /></RequireRole>} />
+
             <Route path="/customer/bookings" element={<RequireRole allow={['customer']}><CustomerAccountBookingsPage /></RequireRole>} />
             <Route path="/customer/profile" element={<RequireRole allow={['customer']}><CustomerProfilePage /></RequireRole>} />
+
             <Route path="/:slug/service/:serviceId" element={<CustomerServiceDetailPage />} />
-            <Route path="/admin/refunds" element={<RequireRole allow={['admin']}><AdminRefundsPage /></RequireRole>} />
             <Route path="/:slug/book" element={<RequireRole allow={['customer']}><CustomerSelectServicePage /></RequireRole>} />
             <Route path="/:slug/book/time" element={<RequireRole allow={['customer']}><CustomerBookingTimePage /></RequireRole>} />
             <Route path="/:slug/book/pay" element={<RequireRole allow={['customer']}><CustomerPaymentPage /></RequireRole>} />
-            <Route path="/:slug/appointments" element={<CustomerAppointmentsPage />} />
             <Route path="/:slug" element={<CustomerHomePage />} />
           </Routes>
         </Router>
