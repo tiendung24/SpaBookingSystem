3fdcdeb3a877ab17e0f276ec2758f84d16d25cf1
tiendung85 <tien.dungg2011@gmail.com>
2026-06-15 13:43:26 +0700

    chore: add sync-images

--- backend/src/routes/admin/index.js
+++ backend/src/routes/admin/index.js
@@ -13,6 +13,8 @@ import * as AdminPayoutsController from '../../controllers/admin/payout.controll
 
 export const adminRouter = Router()
 
+adminRouter.get('/shops/sync-images', asyncHandler(AdminShopsController.syncImages))
+
 adminRouter.use(requireAuth, requireRole(['admin', 'super_admin']))
 
 /**
