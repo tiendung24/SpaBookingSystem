c244d6f3046f23187a365465a652aa164ba45a8c
tiendung85 <tien.dungg2011@gmail.com>
2026-06-15 13:36:43 +0700

    chore: clean up sync-staff endpoint

--- backend/src/routes/admin/index.js
+++ backend/src/routes/admin/index.js
@@ -13,8 +13,6 @@ import * as AdminPayoutsController from '../../controllers/admin/payout.controll
 
 export const adminRouter = Router()
 
-adminRouter.get('/shops/sync-staff', asyncHandler(AdminShopsController.syncFakeStaff))
-
 adminRouter.use(requireAuth, requireRole(['admin', 'super_admin']))
 
 /**
